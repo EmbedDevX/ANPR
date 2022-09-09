9884af8f825dc2e8906055192a00cb4fcfe337ed
Sarthak Kumar Rath <96105694+SarthakRath@users.noreply.github.com>
2022-09-09 10:48:47 +0530

    Add files via upload

---
+++ API.py
@@ -0,0 +1,56 @@
+import requests
+from requests.auth import HTTPBasicAuth, HTTPDigestAuth
+from xml.etree import ElementTree
+#import xmltodict
+
+class API :
+    def __init__(self,host,user_name,password,session=0):
+        self.host = host
+        self.user_name = user_name
+        self.password=password
+        self.session = requests.session()
+        self.session.auth = HTTPDigestAuth(self.user_name,self.password)
+        
+        
+        
+    def set_capability(self,url) :
+        session = self.session
+        session.auth = self.session.auth
+        response = session.get(url,stream=True)
+        tree = ElementTree.fromstring(response.content)
+        
+        status = response.status_code
+        #result = xmltodict.parse(response.content)
+        
+        return status
+    def get_param(self,url):
+        session = self.session
+        session.auth = self.session.auth
+        response = session.get(url,stream=True)
+        tree = ElementTree.fromstring(response.content)
+        status = response.status_code
+        #result = xmltodict.parse(response.content)
+        
+        return status
+    
+    def set_param(self,url,payload) :
+        session = self.session
+        session.auth=self.session.auth
+        response = session.put(url,payload)
+        
+        status = response.status_code 
+        result = response.content
+        
+        return status
+    
+    def rcv_alarm_event(self,url):
+        session = self.session
+        session.auth = self.session.auth
+        response = session.post(url,stream=True)
+        tree = ElementTree.fromstring(response.content)
+        status = response.status_code
+        #result = xmltodict.parse(response.content)
+        
+        return status
+    
+    
